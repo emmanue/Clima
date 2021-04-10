5c07302b10b99fe915a53ded8aac9dbc33ddf3c0
EMMANUEL FOTSO KAMGUE <brinkcorp@gmail.com>
2021-04-10 14:28:49 +0000

    decode data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
 import '../services/location.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 
 class LoadingScreen extends StatefulWidget {
@@ -10,6 +11,12 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   // solved an issue with the static call on a field
   void getLocation() async {
     Location location = Location();
@@ -18,8 +25,32 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(location.longitude);
   }
 
+  void getData() async {
+    http.Response response = await http.get("api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid");
+    if (response.statusCode == 200){
+      String data = response.body;
+      
+      // var longitude = jsonDecode(data)['coord']['lon'];
+      // print(longitude);
+      //
+      // var weatherDescription = jsonDecode(data)['weather'][0]['description']
+
+      var decodedData = jsonDecode(data);
+
+      var tempMain = decodedData['main']['temp'];
+
+      var weatherId = decodedData['weather'][0]['id'];
+
+      var nameOfCity = decodedData['name'];
+      
+    } else {
+      print(response.statusCode);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
       body: Center(
         child: RaisedButton(
