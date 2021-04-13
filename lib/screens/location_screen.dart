3029dadf776d99adaea6a41f798985f812d2cb42
EMMANUEL FOTSO KAMGUE <brinkcorp@gmail.com>
2021-04-13 22:40:54 +0000

    weather model

--- lib/screens/location_screen.dart
+++ lib/screens/location_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../utilities/constants.dart';
+import '../services/weather.dart';
 
 class LocationScreen extends StatefulWidget {
 
@@ -13,9 +14,11 @@ class LocationScreen extends StatefulWidget {
 
 class _LocationScreenState extends State<LocationScreen> {
 
+  WeatherModel weather = WeatherModel();
   int temperature;
-  int condition;
+  String weatherIcon;
   String cityName;
+  String weatherMessage;
 
   @override
   void initState() {
@@ -25,11 +28,14 @@ class _LocationScreenState extends State<LocationScreen> {
   }
 
   void updateUI(dynamic weatherData){
-
-     double temp = weatherData['main']['temp'];
-     temperature = temp.toInt();
-     condition = weatherData['weather'][0]['id'];
-     cityName = weatherData['name'];
+    setState(() {
+       double temp = weatherData['main']['temp'];
+       var temperature = temp.toInt();
+       var condition = weatherData['weather'][0]['id'];
+       weatherIcon = weather.getWeatherIcon(condition);
+       weatherMessage = weather.getMessage(temperature);
+       cityName = weatherData['name'];
+    });
   }
 
   @override
@@ -78,7 +84,7 @@ class _LocationScreenState extends State<LocationScreen> {
                       style: kTempTextStyle,
                     ),
                     Text(
-                      '☀️',
+                      weatherIcon,
                       style: kConditionTextStyle,
                     ),
                   ],
@@ -87,7 +93,7 @@ class _LocationScreenState extends State<LocationScreen> {
               Padding(
                 padding: EdgeInsets.only(right: 15.0),
                 child: Text(
-                  "It's 🍦 time in San Francisco!",
+                  '$weatherMessage in $cityName!',
                   textAlign: TextAlign.right,
                   style: kMessageTextStyle,
                 ),
